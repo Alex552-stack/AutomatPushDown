28b2ce3f24234520d0508538d4907b261fe6c4ff
Alex <avramalexandru2030@gmail.com>
2024-11-04 17:15:28 +0200

    Gata tema 2

---
+++ LR1Parser/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Text.Json;
+using Tema1Limbaje.Models;
+
+InputModel model = 
+    JsonSerializer.Deserialize<InputModel>(
+        File.ReadAllText("input.json")
+    );
+
+ParsingTableInput parsingTableInput = JsonSerializer.Deserialize<ParsingTableInput>(File.ReadAllText("Table.json"));
+ParsingTable parsingTable = new ParsingTable(parsingTableInput);
+
+var parser = new Parser(parsingTable);
+parser.Parse("i+i*i");
+parser.PrintSteps();
---
+++ Lr21Incercarea2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using Lr21Incercarea2.Tables;
+using Tema1Limbaje.Models;
+
+
+InputModel model = JsonSerializer.Deserialize<InputModel>(File.ReadAllText("input.json"));
+TableInputModel tableModel = JsonSerializer.Deserialize<TableInputModel>(File.ReadAllText("Tables.json"));
+
+Table actionTable = new Table(tableModel.ActionTable);
+Table goToTable = new Table(tableModel.GotoTable);
+
+Lr1Parser.Parse(model, actionTable,goToTable);
